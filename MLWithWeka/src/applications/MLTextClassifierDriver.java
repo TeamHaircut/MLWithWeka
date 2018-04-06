a18ecddbc3a071ed1787000d7478ae475d1d8677
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-04-05 21:14:49 -0400

    updated text classifier driver

--- MLWithWeka/src/applications/MLTextClassifierDriver.java
+++ MLWithWeka/src/applications/MLTextClassifierDriver.java
@@ -21,6 +21,7 @@ public class MLTextClassifierDriver extends Application{
 	        
 	        primaryStage.setTitle("ML Text Classifier");
 	        primaryStage.setScene(scene);
+	        primaryStage.setResizable(false);
 	        primaryStage.show();
 	    }
 	 public static void main(String[] args) {
