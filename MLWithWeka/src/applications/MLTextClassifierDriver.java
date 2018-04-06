f8c8ab8c079f7a7fd8d950584b9622cd48cfb1df
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-04-05 20:38:08 -0400

    added ml text classifier driver

---
+++ MLWithWeka/src/applications/MLTextClassifierDriver.java
@@ -0,0 +1,30 @@
+package applications;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MLTextClassifierDriver extends Application{
+	
+	 @Override
+	    public void start(Stage primaryStage) throws Exception {
+
+	    	Node simple = FXMLLoader.load(getClass().getResource("/view0.fxml"));
+			
+	        StackPane root = new StackPane();
+	        root.getChildren().add(simple);
+	        
+	        Scene scene = new Scene(root, 300, 250);
+	        
+	        primaryStage.setTitle("ML Text Classifier");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	    }
+	 public static void main(String[] args) {
+	        launch(args);
+	    }
+
+}
