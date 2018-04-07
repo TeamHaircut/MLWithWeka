c2d2c1572075f3d1b9826edc20446210c4b3d600
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-04-06 20:52:04 -0400

    updated text classifier controller

--- MLWithWeka/src/controllers/TextClassifierController.java
+++ MLWithWeka/src/controllers/TextClassifierController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
+import supportclasses.Classify;
+import supportclasses.ToArff;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -18,11 +24,37 @@ public class TextClassifierController {
 	@FXML private FileChooser fileChooser = new FileChooser();
     @FXML private ComboBox<String> modelCB = new ComboBox<>();
     @FXML private TextField outputTF = new TextField();
-    @FXML private Button predictBTN;
+    @FXML private Button classifyBTN;
     @FXML private TextArea textTA;
     @FXML private TextField fileTF;
     @FXML private Button fileBTN;
     @FXML private Button resetBTN;
+    
+    @FXML
+    private void classifyBTNAction() {
+    	System.out.println("Button Pressed");
+    	PrintWriter out = null;
+        String input = textTA.getText();
+        try {
+            out = new PrintWriter("cache.txt", "UTF-8");
+            out.println(input);
+        } catch (FileNotFoundException | UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+
+        out.close();
+        
+        ToArff toArff = new ToArff("cache.txt");
+        toArff.convert();
+
+        Classify classifier = new Classify();
+        classifier.loadModel(textClassifierModel.getModel());
+        System.out.println("using model: "+ textClassifierModel.getModel());
+        classifier.classify("output.arff");
+
+        classifier.printTestClasses();
+        outputTF.setText(classifier.classificationsAsString());
+    }
 	
     @FXML void initialize(){
     	if(modelCB != null) {
@@ -30,11 +62,8 @@ public class TextClassifierController {
         	modelCB.getItems().addAll("Naive Bayes Multinominal Text","Customized Naive Bayes Multinominal");
             modelCB.setValue("Naive Bayes Multinominal Text");
             
-            Map<String,String> modelMap = new HashMap<String,String>();
-            modelMap.put("Naive Bayes Multinominal Text", "classBalancedNBMT.model");
-            modelMap.put("Customized Naive Bayes Multinominal", "classBalancedSTWVNBM.model");
-            
             modelCB.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            	textClassifierModel.setCBSelection(newValue);
 		    	System.out.println(newValue+" selected");
 		    });
     		
